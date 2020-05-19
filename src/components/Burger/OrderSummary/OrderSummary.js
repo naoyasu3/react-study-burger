c7706c8359ac2581cf93418b2d1de6c32b89679d
naoyasu3 <watanabe_naoyasu@lightcafe.co.jp>
2020-05-19 16:52:19 +0900

    注文確認画面に、合献金額表示

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -18,6 +18,9 @@ const OrderSummary = (props) => {
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to Checkout?</p>
+      <p>
+        <strong>Total Price: {props.price}</strong>
+      </p>
       <Button btnType="Danger" clicked={props.purchaseCanceled}>
         キャンセル
       </Button>
